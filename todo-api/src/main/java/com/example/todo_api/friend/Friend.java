786d316e527a2f829063fa526ac1791e9047d2dc
jay91537 <veuxft@gmail.com>
2024-10-08 01:23:58 +0900

    Feat 3rd study

---
+++ todo-api/src/main/java/com/example/todo_api/friend/Friend.java
@@ -0,0 +1,31 @@
+package com.example.todo_api.friend;
+
+import com.example.todo_api.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Friend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "friend_id")
+    private Long id;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member request_member;
+
+    @JoinColumn(name = "friend_member_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member Response_member;
+
+    public Friend(Member request_member, Member response_member) {
+        this.request_member = request_member;
+        Response_member = response_member;
+    }
+}
